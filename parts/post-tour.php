315ebc0c2849c6151c09c2db1e975f36cf2b55b4
Pradheep Nair <pradheep.pnair@gmail.com>
2026-02-08 12:24:58 +0400

    Update post-tour.php

--- parts/post-tour.php
+++ parts/post-tour.php
@@ -12,12 +12,14 @@
 
 global $wp_query; 
 $post = $wp_query->post; 
-$page = get_page_by_path('tour', OBJECT, 'page'); 
+$page = get_page_by_path('tours', OBJECT, 'page'); 
 if(!$page) {
     wp_redirect(home_url()); 
     exit;
 }
-$page_title = $post->post_title;
+$page_title = $page->post_title;
+$page_url = get_permalink($page->ID);
+$tour_title = $post->post_title;
 $page_image_url = get_field('header_image', $post->ID);
 $duration = get_field('duration', $post->ID);
 ?>
@@ -30,8 +32,9 @@ $duration = get_field('duration', $post->ID);
                     <h1 class="mb-3"><?php echo $page_title; ?></h1>
                     <nav aria-label="breadcrumb" class="d-block">
                         <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="<?php echo $_c('HOST_URL'); ?>">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><?php echo $page_title; ?></li>
+                            <li class="breadcrumb-item"><a href="<?php echo DPA_THEME_URI; ?>">Home</a></li>
+                            <li class="breadcrumb-item"><a href="<?php echo $page_url; ?>"><?php echo $page_title; ?></a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><?php echo $tour_title; ?></li>
                         </ul>
                     </nav>
                 </div>
@@ -48,7 +51,7 @@ $duration = get_field('duration', $post->ID);
                     <div class="single-content">
                         <div class="single-full-title border-b mb-2 pb-2">
                             <div class="single-title">
-                                <h2 class="mb-1"><?php echo $page_title; ?></h2>
+                                <h2 class="mb-1"><?php echo $tour_title; ?></h2>
                                 <div class="rating-main d-lg-flex align-items-center text-lg-start text-center">
                                     <p class="mb-0 me-2"><i class="icon-clock"></i> <?php echo $duration; ?></p> 
                                 </div>

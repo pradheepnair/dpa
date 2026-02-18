84213c73f0b5c3fceb8786770e79b947de24d658
Pradheep Nair <pradheep.pnair@gmail.com>
2026-02-18 14:17:07 +0400

    Update common.php

--- assets/inc/common.php
+++ assets/inc/common.php
@@ -689,7 +689,7 @@ function form_action() {
                     </div> 
                     <div class="row">
                         <div class="col-md-12">
-                            <h2>Please enter your details below</h2>
+                            <h2>Thank you!</h2>
                         </div>
                     </div>
                 </form>';

d90b63a198bd49ca4d73b85c7c45930f485300ff
Pradheep Nair <pradheep.pnair@gmail.com>
2026-03-11 14:36:02 +0400

    Update template-about.php

--- templates/template-about.php
+++ templates/template-about.php
@@ -11,6 +11,25 @@ global $wp_query;
 get_header(); 
 $about_image_url = get_the_post_thumbnail_url($post->ID, 'full');
 ?>
+    <!-- BreadCrumb Starts -->  
+    <section class="breadcrumb-main pb-20 pt-14" style="background-image: url(<?php echo DPA_THEME_URI; ?>/assets/images/bg/about.jpg);">
+        <div class="section-shape section-shape1 top-inherit bottom-0" style="background-image: url(<?php echo DPA_THEME_URI; ?>/assets/images/shapes/shape8.png);"></div>
+        <div class="breadcrumb-outer">
+            <div class="container">
+                <div class="breadcrumb-content text-center">
+                    <h1 class="mb-3">About Us</h1>
+                    <nav aria-label="breadcrumb" class="d-block">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?php echo get_home_url(); ?>">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">About Us</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="dot-overlay"></div>
+    </section>
+    <!-- BreadCrumb Ends --> 
 <section class="about-us pt-6" style="background-image:url(<?php echo DPA_THEME_URI; ?>/assets/images/bg/background_pattern.png); background-position:bottom right;">
     <div class="container">
         <div class="about-image-box">

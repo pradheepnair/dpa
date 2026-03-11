3cec258807d0b55d9d242147c8ee0282d4b31304
Pradheep Nair <pradheep.pnair@gmail.com>
2026-03-11 15:37:05 +0400

    Update template-gallery.php

--- templates/template-gallery.php
+++ templates/template-gallery.php
@@ -34,23 +34,31 @@ $about_image_url = get_the_post_thumbnail_url($post->ID, 'full');
     <div class="gallery pb-0 pt-6">
         <div class="container">
             <div class="section-title mb-6 text-center w-75 mx-auto">
-                <?php echo $post->post_content; ?>
-                <h4 class="mb-1 theme1">Our Gallery</h4>
-                <h2 class="mb-1">Some Beautiful <span class="theme">Snapshoots</span></h2> 
+                <?php echo $post->post_content; ?> 
             </div>
-            <?php if (count($images) > 0) { ?>
+            <?php
+            $args = array(
+                'post_type' => 'image',
+                'posts_per_page' => -1,
+                'orderby' => array('menu_order' => 'ASC'),
+            );
+            $images = get_posts($args);
+            $total_images = count($images);
+            if ($total_images > 0) {  ?>
             <div class="row">
-                <?php foreach($images as $image) { ?>
+                <?php foreach($images as $image) { 
+                    $image_url = get_the_post_thumbnail_url($image->ID, 'full');
+                    $image_title = $image->post_title;
+                    ?>
                 <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
                     <div class="gallery-item mb-4 rounded overflow-hidden">
                         <div class="gallery-image">
-                            <img src="<?php echo HOST_URL; ?>/assets/images/2023/gallery/<?php echo $image['image_url']; ?>" alt="image">
+                            <img src="<?php echo $image_url; ?>" alt="image">
                             <div class="overlay"></div>
                         </div>
-                        <div class="gallery-content">
-                            <!--<h5 class="white text-center position-absolute bottom-0 pb-4 left-50 mb-0 w-100">Barcelona - Spain</h5>-->
+                        <div class="gallery-content"> 
                             <ul>
-                                <li><a href="<?php echo HOST_URL; ?>/assets/images/2023/gallery/<?php echo $image['image_url']; ?>" data-lightbox="gallery" data-title="Our Gallery"><i class="fa fa-eye"></i></a></li>
+                                <li><a href="<?php echo $image_url; ?>" data-lightbox="gallery" data-title="<?php echo $image_title; ?>"><i class="fa fa-eye"></i></a></li>
                             </ul>
                         </div>
                     </div>

76f4310018f88419bbf942f1eea6e0c76cc12486
Pradheep Nair <pradheep.pnair@gmail.com>
2026-03-11 14:38:19 +0400

    Update template-about.php

--- templates/template-about.php
+++ templates/template-about.php
@@ -43,7 +43,7 @@ $about_image_url = get_the_post_thumbnail_url($post->ID, 'full');
                     </div>
                 </div>
                 <div class="col-lg-12">
-                    <?pbp get_template_part('parts/home-counter', 'home'); ?>
+                    <?pbp get_template_part('../parts/home-counter', 'home'); ?>
                 </div>
             </div>
         </div>

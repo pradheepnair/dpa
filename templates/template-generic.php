26e219bbb45e524c163c3bb434e8a6bab2bdc68a
Pradheep Nair <pradheep.pnair@gmail.com>
2026-03-12 10:49:45 +0400

    generic page

--- templates/template-generic.php
+++ templates/template-generic.php
@@ -33,10 +33,10 @@ $generic_image_url = get_the_post_thumbnail_url($post->ID, 'full');
     <!-- page starts -->
     <section class="about-us pt-6" style="background-image:url(<?php echo DPA_THEME_URI; ?>/assets/images/bg/background_pattern.png); background-position:bottom right;">
         <div class="container">
-            <div class="about-image-box">
+            <div class="generic-content-box">
                 <div class="row d-flex align-items-center justify-content-between">
                     <div class="col-lg-12">
-                        <div class="about-content text-center text-lg-start"> 
+                        <div class="text-lg-start"> 
                             <?php echo $post->post_content; ?>
                         </div>
                     </div> 

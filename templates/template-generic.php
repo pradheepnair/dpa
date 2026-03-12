aa47fd4adde42535392292f227e8a6108928300c
Pradheep Nair <pradheep.pnair@gmail.com>
2026-03-12 10:32:03 +0400

    Create template-generic.php

---
+++ templates/template-generic.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Template Name: Generic
+ *
+ * @package     Dubai Private Adventure Theme
+ * @author      Pradheep Nair <pradheep.pnair@gmail.com>
+ * @since       1.0.0
+ */
+
+global $wp_query; 
+get_header(); 
+$generic_image_url = get_the_post_thumbnail_url($post->ID, 'full');
+?>
+    <!-- BreadCrumb Starts -->  
+    <section class="breadcrumb-main pb-20 pt-14" style="background-image: url(<?php echo $generic_image_url; ?>);">
+        <div class="section-shape section-shape1 top-inherit bottom-0" style="background-image: url(<?php echo DPA_THEME_URI; ?>/assets/images/shapes/shape8.png);"></div>
+        <div class="breadcrumb-outer">
+            <div class="container">
+                <div class="breadcrumb-content text-center">
+                    <h1 class="mb-3"><?php echo $post->post_title; ?></h1>
+                    <nav aria-label="breadcrumb" class="d-block">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?php echo get_home_url(); ?>">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><?php echo $post->post_title; ?></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="dot-overlay"></div>
+    </section>
+    <!-- BreadCrumb Ends --> 
+    <!-- about-us starts -->
+    <section class="about-us pt-6" style="background-image:url(<?php echo DPA_THEME_URI; ?>/assets/images/bg/background_pattern.png); background-position:bottom right;">
+        <div class="container">
+            <div class="about-image-box">
+                <div class="row d-flex align-items-center justify-content-between">
+                    <div class="col-lg-12">
+                        <div class="about-content text-center text-lg-start"> 
+                            <?php echo $post->post_content; ?>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+        <div class="white-overlay"></div>
+    </section>
+    <!-- about-us ends --> 
+<?php get_footer(); ?>
